3121fb627cacec749af94ec9703e80a931c7bad8
Inigo Orosco <enyeahgo@gmail.com>
2026-05-03 15:42:17 +0800

    Fix sqlite path and session store for production

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const moment = require('moment-timezone');
 const { v4: uuidv4 } = require('uuid');
 const expressLayouts = require('express-ejs-layouts');
 const axios = require('axios');
-const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'));
+// Burahin ang 'const' sa unahan kung may na-declare na sa taas
+global.db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'));
+const db = global.db;
 const app = express();
 const PORT = process.env.PORT || 3004;
 const AGENT_COMMISSION_RATE = 0.03;
